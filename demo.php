8e65012f48bfa8864572e3db74de3fafae79708b
Epi <ambylise12@gmail.com>
2017-04-23 18:20:46 +0200

    animación de prueba

---
+++ demo.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Epi
+ * Date: 23/04/2017
+ * Time: 13:37
+ */
+
+    $datos = ['dato1'=>"hola",
+        'dato2'=>"hola",
+        'dato3'=>"hola",
+        'dato4'=>"hola",
+        ];
+    function hola(){
+
+    }
+
+
+?>
